4abfb73f9b1e02b1a2ea7a42671ac2894e279882
tiiildaolsson1 <tiiildaolsson@gmail.com>
2025-05-12 14:43:34 +0200

    Mitt egna jest test, fail version

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,8 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('should have a function called push', () => {
+    expect(typeof stack.push).toBe('number');  
+  });
+  
